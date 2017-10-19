06dd9ac16f6245c7bd02b529bda4bc4ee15bc33c
Mat Moradi <matmoradi17@gmail.com>
2017-10-19 13:58:43 -0700

    Removed partserver From Master Branch
    This development has been moved to a protected branch.

--- partserver/db.rb
+++
@@ -1,9 +0,0 @@
-# Copyright 2013 Team 254. All Rights Reserved.
-# @author pat@patfairbank.com (Patrick Fairbank)
-#
-# Sets up database connection.
-
-require "cheesy-common"
-
-DB = Sequel.mysql2({ :host => CheesyCommon::Config.db_host, :user => CheesyCommon::Config.db_user,
-	:password => CheesyCommon::Config.db_password, :database => CheesyCommon::Config.db_database })
