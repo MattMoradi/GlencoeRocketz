b815b121891b379c9486bb2c7fd5f1c2474c884e
joshet1223 <josherikt@gmail.com>
2018-05-14 15:25:20 -0700

    Create Gyroandcompass.ino

---
+++ Arduino Files/Gyroandcompass.ino
@@ -0,0 +1,57 @@
+#include <LSM303.h>
+
+/*
+The sensor outputs provided by the library are the raw 16-bit values
+obtained by concatenating the 8-bit high and low gyro data registers.
+They can be converted to units of dps (degrees per second) using the
+conversion factors specified in the datasheet for your particular
+device and full scale setting (gain).
+
+Example: An L3GD20H gives a gyro X axis reading of 345 with its
+default full scale setting of +/- 245 dps. The So specification
+in the L3GD20H datasheet (page 10) states a conversion factor of 8.75
+mdps/LSB (least significant bit) at this FS setting, so the raw
+reading of 345 corresponds to 345 * 8.75 = 3020 mdps = 3.02 dps.
+
+You will need to install the l3G and LSM303 code from pololu
+*/
+
+#include <Wire.h>
+#include <L3G.h>
+LSM303 compass;
+L3G gyro;
+char report[80];
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+
+  if (!gyro.init())
+  {
+    Serial.println("Failed to autodetect gyro type!");
+    while (1);
+  }
+
+  gyro.enableDefault();
+  compass.init();
+  compass.enableDefault();
+}
+
+void loop() {
+  gyro.read();
+
+  Serial.print("G ");
+  Serial.print("X: ");
+  Serial.print((int)gyro.g.x);
+  Serial.print(" Y: ");
+  Serial.print((int)gyro.g.y);
+  Serial.print(" Z: ");
+  Serial.println((int)gyro.g.z);
+    compass.read();
+
+  snprintf(report, sizeof(report), "A: %6d %6d %6d    M: %6d %6d %6d",
+    compass.a.x, compass.a.y, compass.a.z,
+    compass.m.x, compass.m.y, compass.m.z);
+  Serial.println(report);
+
+  delay(100);
+}
